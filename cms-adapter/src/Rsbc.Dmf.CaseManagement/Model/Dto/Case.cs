ff46fc2c02a99835192867c29d2bde70870e22ec
Jebb Burditt <jebb.burditt@gmail.com>
2024-06-21 09:47:19 -0700

    add todo duedate rename

--- cms-adapter/src/Rsbc.Dmf.CaseManagement/Model/Dto/Case.cs
+++ cms-adapter/src/Rsbc.Dmf.CaseManagement/Model/Dto/Case.cs
@@ -10,6 +10,7 @@ namespace Rsbc.Dmf.CaseManagement.Dto
         // TODO Rename to IDCode
         public string IdCode { get; set; }
 
+        // TODO rename DueDate
         // dfp_latestcompliancedate
         public DateTimeOffset? LatestComplianceDate { get; set; }
 
